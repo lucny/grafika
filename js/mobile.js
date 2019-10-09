f3b93d81e6b7ae0df9abb482c6d9945f28e31c2d
lucny <lucny@sspu-opava.cz>
2019-10-09 23:56:02 +0200

    mobile commit

--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,7 @@
 let sensor = new Accelerometer();
 sensor.addEventListener('reading', function(e) {
     console.log(e.target);
-    cursor.y += Math.round(e.target.y);
-    cursor.x += Math.round(e.target.z);
+    cursor.y += Math.round(Math.sqrt(e.target.y));
+    cursor.x += Math.round(Math.sqrt(e.target.x));
 });
 sensor.start();
